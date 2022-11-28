a43bfda8227444bbdf0d0387c4c6fb8c3c768f2b
Felipe Volpato <fevooduck@gmail.com>
2022-11-28 19:49:03 -0300

    Base api graficos

---
+++ legend.js
@@ -0,0 +1,17 @@
+const legend = {
+  "date: "Data",
+  "weight: "Peso",
+  "imc: "IMC",
+  "body_fat: "Gordura Corporal",
+  "muscle_rate: "Taxa muscular",
+  "fat_free_dough: "Massa livre de gordura",
+  "subcutaneous_fat: "Gordura Subcutânea",
+  "visceral_fat: "Gordura visceral",
+  "body_water: "Água corporal",
+  "skeletal_muscle_mass: "Massa Muscular Esquelética",
+  "muscle_mass: "Massa Muscular",
+  "bone_mass: "Massa Óssea",
+  "protein: "Proteína",
+  "tmb: "TMB",
+  "metabolic_age: "Idade Metabólica"
+}
---
+++ pages/api/infos.js
@@ -0,0 +1,30 @@
+
+import credentials from '../../credentials/google-sheets-api.json';
+import { GoogleSpreadsheet  } from 'google-spreadsheet';
+
+
+
+export default async function handler(req, res) {
+  const doc = new GoogleSpreadsheet('1Y72SkakFPHVnyvKxS_CLnhRymPhrYxw4uJGwiVsDpug');
+
+  await doc.useServiceAccountAuth({
+    // client_email: credentials.client_email,
+    // private_key: credentials.private_key,
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
+
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+
+  const rows = await sheet.getRows();
+
+  const infos = rows.map( ({date, weight, imc, body_fat, muscle_rate, fat_free_dough,subcutaneous_fat, visceral_fat,body_water, skeletal_muscle_mass, muscle_mass,bone_mass,protein,tmb,metabolic_age })=>{
+    return {date, weight, imc, body_fat, muscle_rate, fat_free_dough,subcutaneous_fat, visceral_fat,body_water, skeletal_muscle_mass, muscle_mass,bone_mass,protein,tmb,metabolic_age }
+  })
+
+  res.send({
+    title: doc.title,
+    infos,
+  })
+}
